13fc40f7cb4cbbae1f036dcaed36be86821ce87f
pkogolo <onyivana07@gmail.com>
2023-10-24 03:59:17 +0100

    Roll back to original backend api code

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
+#THE ORIOGINAL CODE THAT WORKS BELOW
 
 from flask import Flask
 import requests
 from bs4 import BeautifulSoup
+import json
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
-
-# Base URL for news details
-base_url = "https://www.nfl.com/news/"  # Replace with your actual URL
 
 @app.route('/')
 @cross_origin(supports_credentials=True)
@@ -16,63 +15,15 @@ def nfl_news():
     nfl_url = "https://www.nfl.com/news/"
     nfl_response = requests.get(nfl_url)
     nfl_soup = BeautifulSoup(nfl_response.content, "html.parser")
-    nfl_news_titles = nfl_soup.find_all("h3", class_="d3-o-media-object__title")
+    nfl_news_titles = nfl_soup.find_all("h3", class_ ="d3-o-media-object__title")
 
     news_headlines = []
-
     for title in nfl_news_titles:
-        title_text = title.text.strip()
-        title_link = base_url + title_text.replace(" ", "-")
-        news_headlines.append(f'<a href="{title_link}">{title_text}</a>')
+        title_text = title.text.strip()  
+        news_headlines.append(title_text)
+       
 
-    # Join the HTML hyperlinks with line breaks
-    news_html = "<br>".join(news_headlines)
-
-    return news_html
-
-if __name__ == "__main":
-    app.run(host='0.0.0.0', port=9000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-#THE ORIOGINAL CODE THAT WORKS BELOW
-
-# from flask import Flask
-# import requests
-# from bs4 import BeautifulSoup
-# import json
-# from flask_cors import CORS, cross_origin
-
-# #flask is not cooperating with me tonight
-# app = Flask(__name__)
-# CORS(app, support_credentials=True)
-
-# @app.route('/')
-# @cross_origin(supports_credentials=True)
-# def nfl_news():
-#     nfl_url = "https://www.nfl.com/news/"
-#     nfl_response = requests.get(nfl_url)
-
-#     nfl_soup = BeautifulSoup(nfl_response.content, "html.parser")
-
-#     nfl_news_titles = nfl_soup.find_all("h3", class_ ="d3-o-media-object__title")
-
-#     news_headlines = []
-#     for title in nfl_news_titles:
-#         news_headlines.append(title.text.strip())
-#         #print(title.text.strip())
-
-#     return json.dumps(news_headlines) 
+    return json.dumps(news_headlines) 
 
 
 # if __name__ == "__main__":
